ee74ee53271b899f4639bc30c77ff93f8d6c573f
Igor <tararuev65@gmail.com>
2023-02-08 00:45:55 +0600

    Z_030sem

---
+++ StW_030sem/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+Console.Clear();
+
+int[] getArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void printArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+
+        Console.Write($"{inArray[i]} ");
+}
+
+int[] array = getArray();
+printArray(array);
