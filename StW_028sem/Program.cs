7f9a030adb3e7293cc30127d89f7d0c7d0e197d7
Igor <tararuev65@gmail.com>
2023-02-08 00:35:14 +0600

    Z_028sem

--- StW_028sem/Program.cs
+++ StW_028sem/Program.cs
@@ -2,4 +2,17 @@
 
 Console.Clear();
 
-int ProdNumb;
+int Multi(int numb);
+{
+    prod = 1;
+    for (int i =1; i<numb; i++)
+    {
+         prod *= i;
+      
+    }
+    return prod;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int N =int.Parse(Console.ReadLine()!);
+Console.Write($"Произведение чисел от 1 до{N} равно {Multi(N)}");
