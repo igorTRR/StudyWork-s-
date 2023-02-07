4277aa0592c5d69117ec28ca1c6405fbb63fe1b4
Igor <tararuev65@gmail.com>
2023-02-07 23:50:52 +0600

    Z_026sem

---
+++ StW_026sem/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//  456 -> 3 78 -> 2 89126 -> 5
+
+
+int DigitCount(int A)
+{
+    int counnt = 0;
+    while(A!= 0)
+    {
+        A/=10;
+        counnt ++;
+    } 
+    return counnt;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(DigitCount(N));
