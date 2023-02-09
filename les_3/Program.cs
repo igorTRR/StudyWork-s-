ebb362ab1b82fe055e23af0284ad981bf8430346
Igor <tararuev65@gmail.com>
2023-02-09 12:36:55 +0600

    ZdL-3

---
+++ les_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// найти мах и 9 чисел
+
+int a1 = 72;
+int b1 = 3;
+int c1 = 54;
+int a2 = 5;
+int b2 = 16;
+int c2 = 7;
+int a3 = 131;
+int b3 = 12;
+int c3 = 45;
+
+int max=a1;
+
+if(b1>max) max=b1;
+if(c1>max) max=c1;
+
+
+if(a2>max) max=a2;
+if(b2>max) max=b2;
+if(c2>max) max=c2;
+
+if(a3>max) max=a3;
+if(b3>max) max=b3;
+if(c3>max) max=c3;
+
+Console.WriteLine(max);
+
+
