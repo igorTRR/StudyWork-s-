8b23ecf50219f4cfe50ad6c11bb26904d234526f
Igor <tararuev65@gmail.com>
2023-02-09 14:04:21 +0600

    Z_008_les

--- les_3/Program.cs
+++ les_3/Program.cs
@@ -1,28 +1,32 @@
 ﻿// найти мах и 9 чисел
 
+ int Max( int arg1, int arg2, int arg3)
+ {
+    int result = arg1;
+
+    if (arg2>result) result =arg2;
+    if (arg3>result) result =arg3;
+    return result;
+}
+
 int a1 = 72;
 int b1 = 3;
-int c1 = 54;
-int a2 = 5;
-int b2 = 216;
-int c2 = 7;
+int c1 = 6754;
+ int a2 = 5;
+ int b2 = 4216;
+ int c2 = 7;
 int a3 = 131;
 int b3 = 12;
-int c3 = 1145;
+ int c3 = 1145;
 
-int max=a1;
+// int max1= Max(a1,b1,c1);
+// int max2= Max(a2,b2,c2);
+// int max3= Max(a3,b3,c3);
+// int max =Max( max1, max2, max3);
+int max =Max( Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
 
-if(b1>max) max=b1;
-if(c1>max) max=c1;
 
 
-if(a2>max) max=a2;
-if(b2>max) max=b2;
-if(c2>max) max=c2;
-
-if(a3>max) max=a3;
-if(b3>max) max=b3;
-if(c3>max) max=c3;
 
 Console.WriteLine(max);
 
