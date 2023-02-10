3e5f1a47deaed512c012079aae458710ca5c8510
Igor <tararuev65@gmail.com>
2023-02-10 14:05:35 +0600

    Zd_027cem2

---
+++ Sem_4-Zd_27sem/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine()!);
+int K = N % 10;
+int Sum = K;
+while (N != 0)
+{
+    N /= 10;
+    K = N % 10;
+    Sum = Sum + K;
+}
+Console.WriteLine($"{Sum}"!);
