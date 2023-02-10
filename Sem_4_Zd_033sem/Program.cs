e43df05e1d99f4a3911b2b85563b9aef95afcbb7
Igor <tararuev65@gmail.com>
2023-02-10 16:13:41 +0600

    Zd_33sem

--- Sem_4_Zd_033sem/Program.cs
+++ Sem_4_Zd_033sem/Program.cs
@@ -1,33 +1,35 @@
 ﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 //  4; массив [6, 7, 19, 345, 3] -> нет3; массив [6, 7, 19, 345, 3] -> да
 
-int[]GetArray(int size, int minVolue, int maxVolue)
-{
-    int[] result = new result(size);
-    for(int i = 0; i < size; i++)
-    {
-    int result = new Random().Next( minVolue,  maxVolue+1);
-    } 
-    return result;                       
+ int[]GetArray(int size, int minVolue, int maxVolue)
+ {
+    int[] result = new int[size];
+     for(int i = 0; i < size; i++)
+     {
+     result[i] = new Random().Next( minVolue,  maxVolue+1);
+     } 
+     return result;                       
 }
-int FindeEl( int n, int[] collection);
-{
+ bool FindeEl(int number, int[]collection)
+ {
     foreach (int el in collection)
     {
-        if (el == n)
-        {
+        if (el == number)
+       {
             return true;
-        }
-        
+         }
     }
-    return false;
+     return false;
+ }
+ int[] Charray = GetArray(12, -9, 9); 
+ Console.Write(String.Join(", ",Charray));
+ Console.WriteLine("Ведите число: ");
+ int n = int.Parse(Console.ReadLine()!);
+ if (FindeEl(n, Charray))
+    {
+    Console.Write($"Заданное число ={n} есть");
+    }
+  else
+    {
+    Console.Write($"Заданного числа {n} нет");
 }
-int[] Charray = GetArray(999, -9, 9); 
-Console.Write(String.Join(", ",Charray));
-Console.Write("Ведите число: ");
-int n = int.Parse(ReadLine()!);
-
-Console.Write($"Заданное число {n} есть, числа {n} нет");
-
-
-
