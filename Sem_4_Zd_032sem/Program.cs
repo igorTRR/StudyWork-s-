a312384f39a648ab2d8200143da3337f25b74672
Igor <tararuev65@gmail.com>
2023-02-10 14:33:52 +0600

    Zd_032sem

---
+++ Sem_4_Zd_032sem/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие 
+//отрицательные, и наоборот.
+Console.Clear();
+
+int[] GetArray(int size, int MinValue, int MaxValue)
+{
+    int[] Result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = new Random().Next(MinValue, MaxValue + 1);
+    }
+    return Result;
+}
+
+void sigChange(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+SigChange(array);
+Console.WriteLine(String.Join(", ", array));
