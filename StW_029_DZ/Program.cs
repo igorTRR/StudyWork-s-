82f8d7b27385cd9776416b47059a60fd1796798e
Igor <tararuev65@gmail.com>
2023-02-10 20:12:49 +0600

    StW_029_DZ

---
+++ StW_029_DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(9);
+    }
+    return array;
+}
+void printArray(int[] NewArray)
+{
+    for (int i = 0; i < NewArray.Length; i++)
+        Console.Write($"{NewArray[i]} "!); 
+}
+int[] array = GetArray();
+printArray(array);
