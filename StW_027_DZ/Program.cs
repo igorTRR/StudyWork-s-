68a0673eb05b7d817cffbb5b8375c6ad835126ae
Igor <tararuev65@gmail.com>
2023-02-10 13:44:36 +0600

    Почистил

---
+++ StW_027_DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDig(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number%10;
+        number = number/10;
+    }
+    return result;
+}
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Сумма цифр числа{A} ={SumDig}(result)");
+
--- StW_027_DZ/obj/Debug/net6.0/StW_027_DZ.AssemblyInfo.cs
+++ StW_027_DZ/obj/Debug/net6.0/StW_027_DZ.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("les_3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("StW_027_DZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("les_3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("les_3")]
+[assembly: System.Reflection.AssemblyProductAttribute("StW_027_DZ")]
+[assembly: System.Reflection.AssemblyTitleAttribute("StW_027_DZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
