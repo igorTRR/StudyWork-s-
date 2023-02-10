e6cb3d8bcfbddce2a8cfcd0d0a324c035cb13624
Igor <tararuev65@gmail.com>
2023-02-10 15:34:33 +0600

    Zd_033sem

---
+++ Sem_4_Zd_033sem/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//  4; массив [6, 7, 19, 345, 3] -> нет3; массив [6, 7, 19, 345, 3] -> да
+
+int[]GetArray(int size, int minVolue, int maxVolue)
+{
+    int[] result = new result(size);
+    for(int i = 0; i < size; i++)
+    {
+    int result = new Random().Next( minVolue,  maxVolue+1);
+    } 
+    return result;                       
+}
+int FindeEl( int n, int[] collection);
+{
+    foreach (int el in collection)
+    {
+        if (el == n)
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
+int[] Charray = GetArray(999, -9, 9); 
+Console.Write(String.Join(", ",Charray));
+Console.Write("Ведите число: ");
+int n = int.Parse(ReadLine()!);
+
+Console.Write($"Заданное число {n} есть, числа {n} нет");
+
+
+
