5e63556e56999274bfc43b0072bbcd5e4cc006f6
Igor <tararuev65@gmail.com>
2023-02-11 23:54:31 +0600

    Zd036_DZ

---
+++ Sem-4_Zd036_DZ/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+} 
+int SumEl(int[]array)
+{
+    int i;
+    int sum = 0;
+    for(i =1; i < array.Length; i+=2)
+    {
+       sum = sum+array[i];
+    }
+    return sum;
+}   
+Console.Clear(); 
+int[]array = GetArray(7, 10, 24);
+int N = SumEl(array);
+Console.WriteLine(string.Join(", ",array));
+Console.WriteLine($"Сумма чисел на нечетных позициях масива = {N}");
