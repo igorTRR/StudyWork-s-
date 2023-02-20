5adda6569c3e906ef0e9a5aaf29d89690b74dedc
Igor <tararuev65@gmail.com>
2023-02-21 01:19:02 +0600

    z055sem

---
+++ z055sem/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Transparent(int[,] newArray)
+{
+    int[,] transparent = new int[newArray.GetLength(1), newArray.GetLength(0)];
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            transparent[j, i] = newArray[i, j];
+        }
+    }
+    return transparent;
+}
+
+Console.Clear();
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array2D = GetArray(row, col, -1, 5);
+PrintArray(array2D);
+Console.WriteLine();
+if (row != col)
+{
+    Console.Write("Операцию вывести невозможно");
+}
+else
+{
+    int[,] arrayLast = Transparent(array2D);
+    PrintArray(arrayLast);
+}
