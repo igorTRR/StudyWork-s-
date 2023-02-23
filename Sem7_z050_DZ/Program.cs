ba9739f7cace468404e46d1f82af6863426ca343
Igor <tararuev65@gmail.com>
2023-02-23 10:52:03 +0600

    z050_DZ

--- Sem7_z050_DZ/Program.cs
+++ Sem7_z050_DZ/Program.cs
@@ -16,7 +16,6 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
             result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
-
     return result;
 }
 void PrintArray(int[,] inArray)
@@ -33,14 +32,13 @@ void PrintArray(int[,] inArray)
 string ResivElements(int m, int n, int[,] inArray)
 {
     if( m < 0 || n < 0 ) return "m,n положительные";
-   string result ;
+   string result;
     bool Elements = m <= inArray.GetLength(0)-1 && n <= inArray.GetLength(1)-1;
     if (Elements)
     {
           result =$"{m},{n}-> {inArray[m,n]}";
     }
-
-     else
+    else
     {
      result =$"{m},{n}-> Такой позиции нет";
     }
@@ -50,11 +48,11 @@ Console.Clear();
 
 int row = new Random().Next(3,5);
 int colums = new Random().Next(3,5);
+
 int[,]inArray = GetArray(row, colums, -5, 5);
 PrintArray(inArray);
 Console.WriteLine();
-
-Console.WriteLine("Введите позицию элемента в двухмезном массиве  ");
-int[] position = Array.ConvertAll(Console.ReadLine()!.Split(' '),Convert.ToInt32);
-int m = position(0), n =position(1);
+Console.WriteLine("Введите позицию элемента в двухмезном массиве ");
+int[] position = Array.ConvertAll(Console.ReadLine().Split(' '),Convert.ToInt32);
+int m = position(0), n=position(1);
 Console.WriteLine(ResivElements(m, n, inArray));
