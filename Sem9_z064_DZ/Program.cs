9609c679f1cb3f9ce8ab7b71b7e50df935be17cb
Igor <tararuev65@gmail.com>
2023-02-27 15:29:17 +0600

    z064_DZ

---
+++ Sem9_z064_DZ/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NamberRec(int N)
+{
+    if(N == 1) return $"{N}";
+    return  $"{N},"+ NamberRec(N -1);
+}
+Console.Write("Введите число N: ");
+int N =int.Parse(Console.ReadLine()!);
+Console.WriteLine(NamberRec(N));
